ccfccdb649c0cc217030790c1149cd9b0df2a180
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-06-19 19:35:12 -0400

    Pseudo code added in JS.

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+//Hooking in time from Moment.js
 $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 moment().format("YYYY-MM-DD HH:mm");
+
+//Text written in text area needs to be saved once the save button is clicked.
+
+//The text needs to be saved to the local cache, so that when the user exits the page and comes back, the info is still there.
+
+//Scheduler needs to keep track of time, making the time that has passed, the present time, and future time be identifiable through colors.
+
