32ecb689a7225106fa0743e650f8bfd1a1d51d8b
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-06-19 23:00:51 -0400

    Created timeSensor function. Changed times to military time to match Moment.JS. changed addClass to be camel cased.

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 moment().format("YYYY-MM-DD HH:mm");
 
-$("#9am .textStay").val(localStorage.getItem("9am"));
-$("#10am .textStay").val(localStorage.getItem("10am"));
-$("#11am .textStay").val(localStorage.getItem("11am"));
-$("#12pm .textStay").val(localStorage.getItem("12pm"));
-$("#1pm .textStay").val(localStorage.getItem("1pm"));
-$("#2pm .textStay").val(localStorage.getItem("2pm"));
-$("#3pm .textStay").val(localStorage.getItem("3pm"));
-$("#4pm .textStay").val(localStorage.getItem("4pm"));
-$("#5pm .textStay").val(localStorage.getItem("5pm"));
+// Getting the values of the textarea through the textStay class to keep it on the page as long as its in localStorage
+$("#9 .textStay").val(localStorage.getItem("9"));
+$("#10 .textStay").val(localStorage.getItem("10"));
+$("#11 .textStay").val(localStorage.getItem("11"));
+$("#12 .textStay").val(localStorage.getItem("12"));
+$("#13 .textStay").val(localStorage.getItem("13"));
+$("#14 .textStay").val(localStorage.getItem("14"));
+$("#15 .textStay").val(localStorage.getItem("15"));
+$("#16 .textStay").val(localStorage.getItem("16"));
+$("#17 .textStay").val(localStorage.getItem("17"));
 
 //Text written in text area needs to be saved once the save button is clicked.
 $(".saveBtn").click(function (event) {
@@ -21,8 +22,31 @@ $(".saveBtn").click(function (event) {
     localStorage.setItem(timeData, savedText);
 
 })
+// Scheduler needs to keep track of time, making the time that has passed, the present time, and future time be identifiable through colors.
+// Creating function to compare time frm Moment,js to time in rows.
+function timeSensor() {
+    var currentTime = moment().hours()
+    $(".row").each(function () {
+        const rowTime = parseInt($(this).attr("id"));
+// If statements comparing time parsed from row ids to universal clock, to indicate through clors whether the time has passed, is current, or is yet to come.
+        if (rowTime < currentTime) {
+            $(this).addClass("past");
+        }
+
+    else if (rowTime === currentTime) {
+        $(this).addClass("present");
+    }
+    else {
+        $(this).addClass("future");
+    };
+
+})
+};
+//Calls time sensor function.
+timeSensor();
 
 
 
-//Scheduler needs to keep track of time, making the time that has passed, the present time, and future time be identifiable through colors.
+
+
 
