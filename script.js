a580f600a2c0a4ac4dcccd37c6ffa7a0331b2349
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-06-19 21:10:31 -0400

    created get methods for localstorage of the value put in the text area.

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 moment().format("YYYY-MM-DD HH:mm");
 
 $("#9am .textStay").val(localStorage.getItem("9am"));
+$("#10am .textStay").val(localStorage.getItem("10am"));
+$("#11am .textStay").val(localStorage.getItem("11am"));
+$("#12pm .textStay").val(localStorage.getItem("12pm"));
+$("#1pm .textStay").val(localStorage.getItem("1pm"));
+$("#2pm .textStay").val(localStorage.getItem("2pm"));
+$("#3pm .textStay").val(localStorage.getItem("3pm"));
+$("#4pm .textStay").val(localStorage.getItem("4pm"));
+$("#5pm .textStay").val(localStorage.getItem("5pm"));
 
 //Text written in text area needs to be saved once the save button is clicked.
 $(".saveBtn").click(function (event) {
     // event.preventDefault();
     var savedText = $(this).siblings(".textStay").val();
     var timeData = $(this).parent().attr("id");
+    //The text needs to be saved to the local cache, so that when the user exits the page and comes back, the info is still there.
     localStorage.setItem(timeData, savedText);
 
 })
 
-//The text needs to be saved to the local cache, so that when the user exits the page and comes back, the info is still there.
+
 
 //Scheduler needs to keep track of time, making the time that has passed, the present time, and future time be identifiable through colors.
 
